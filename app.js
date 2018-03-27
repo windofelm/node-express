43cfb5894505cae53372ebee10f4a792ec3c03b5
Recep Hidir <recep.hidir@sebit.com.tr>
2018-03-27 10:45:22 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const axios = require('axios');
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.get('/', function (req, res) {
+    res.render("index", { title: 'Hey Hey Hey!', message: 'Yo Yo'});
+});
+
+app.get('/axios', function (req, res) {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+        .then(function (response) {
+            res.render("blog", { article: response.data});
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+//app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'));
